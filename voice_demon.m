4d005883d36156f58d3270b382c9a604237fbf42
WJTung <jie1202@gmail.com>
2016-06-18 20:40:06 +0800

    change chipmunk and demon by fs

--- voice_demon.m
+++ voice_demon.m
@@ -1,8 +1,3 @@
-wObj=myAudioRead('origin.wav');
-opt = pitchShift('defaultOpt');
-opt.pitchShiftAmount = -6;	% Amount for pitch shift (in semitones)
-opt.method='wsola';		% Method for pitch shifting
-wObj2=pitchShift(wObj, opt, 0);
-% sound(wObj2.signal,wObj2.fs);
+[y, fs] = audioread('origin.wav');
 new_filename = 'demon.wav';
-audiowrite(new_filename, wObj2.signal, wObj2.fs);
+audiowrite(new_filename, y, round(0.75 * fs));
